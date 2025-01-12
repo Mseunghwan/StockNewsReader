7595bae37ad68dfbaf5ea01bc0b189b67ad344bd
Mseunghwan <minccc42710@gmail.com>
2025-01-12 14:30:52 +0900

    first commit 기본설정

---
+++ src/main/java/com/example/FinanceNewsProvider/FinanceNewsProviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.FinanceNewsProvider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceNewsProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceNewsProviderApplication.class, args);
+	}
+
+}
